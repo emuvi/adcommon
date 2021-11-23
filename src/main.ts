27780927e17930344b12ce549664554d279262d1
Éverton M. Vieira <everton.muvi@gmail.com>
2021-11-23 13:08:47 -0300

    prototype development

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+export enum AdActions {
+    SEARCH = "search",
+    INSERT = "insert",
+    EDIT = "edit",
+    DELETE = "delete",
+}
+
+export enum AdModules {
+    BUSINESS = "business",
+    REGION = "region",
+    NATION = "nation",
+    STATE = "state",
+    CITY = "city",
+    DISTRICT = "district",
+    PEOPLE = "people",
+    PEOPLE_GROUP = "people_group",
+    PEOPLE_SUBGROUP = "people_subgroup",
+}
