1cf949e2eca9235a6c3f177b463f91a75ddde35b
Éverton M. Vieira <everton.muvi@gmail.com>
2022-04-13 14:49:48 -0300

    prototype development

--- src/ad-register.ts
+++ src/ad-register.ts
@@ -1,10 +1,16 @@
-import { QinColumn, QinField, QinLine, QinMutantsArm, QinPanel, QinTabs } from "qinpel-cps";
+import {
+  QinColumn,
+  QinField,
+  QinLine,
+  QinMutantsArm,
+  QinPanel,
+  QinTabs,
+} from "qinpel-cps";
 import { AdExpect } from "./ad-expect";
 import { AdField } from "./ad-field";
 import { AdModel } from "./ad-model";
 
 export class AdRegister extends QinPanel {
-
   private _expect: AdExpect;
   private _model: AdModel;
 
@@ -13,51 +19,50 @@ export class AdRegister extends QinPanel {
   private line: QinLine = null;
 
   public constructor(expect: AdExpect, table: string) {
-      super();
-      this._expect = expect;
-      this._model = new AdModel(table);
+    super();
+    this._expect = expect;
+    this._model = new AdModel(table);
   }
 
   public addTab(title: string) {
-      if (this.tabs == null) {
-          this.tabs = new QinTabs();
-          this.tabs.install(this);
-      }
-      this.column = new QinColumn();
-      this.tabs.addTab({title, viewer: this.column});
-      this.line = new QinLine();
-      this.line.install(this.column);
+    if (this.tabs == null) {
+      this.tabs = new QinTabs();
+      this.tabs.install(this);
+    }
+    this.column = new QinColumn();
+    this.tabs.addTab({ title, viewer: this.column });
+    this.line = new QinLine();
+    this.line.install(this.column);
   }
 
   public addLine() {
-      if (this.column == null) {
-          this.column = new QinColumn();
-          this.column.install(this);
-      }
-      this.line = new QinLine();
-      this.line.install(this.column);
+    if (this.column == null) {
+      this.column = new QinColumn();
+      this.column.install(this);
+    }
+    this.line = new QinLine();
+    this.line.install(this.column);
   }
 
   public addView(field: AdField) {
-      this._model.addField(field);
-      if (this.line == null) {
-          this.addLine();
-      }
-      const editor = QinMutantsArm.newEdit(field.kind, field.options);
-      if (field.title) {
-          const viewer = new QinField(field.title, editor);
-          viewer.install(this.line);
-      } else {
-          editor.install(this.line);
-      }
+    this._model.addField(field);
+    if (this.line == null) {
+      this.addLine();
+    }
+    const editor = QinMutantsArm.newEdit(field.kind, field.options);
+    if (field.title) {
+      const viewer = new QinField(field.title, editor);
+      viewer.install(this.line);
+    } else {
+      editor.install(this.line);
+    }
   }
 
-	public get expect(): AdExpect {
-		return this._expect;
-	}
+  public get expect(): AdExpect {
+    return this._expect;
+  }
 
-	public get model(): AdModel {
-		return this._model;
-	}
-
+  public get model(): AdModel {
+    return this._model;
+  }
 }
