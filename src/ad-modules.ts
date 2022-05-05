a10f6cb8419f5fa33763cbe142acff607fa40a24
Éverton M. Vieira <everton.muvi@gmail.com>
2022-05-05 12:48:35 -0300

    prototype development

---
+++ src/ad-modules.ts
@@ -0,0 +1,50 @@
+import { QinAsset } from "qinpel-cps";
+import { AdModule } from "./ad-consts";
+
+export class AdModules {
+  static BUSINESS: AdModule = {
+    app: "adpeople",
+    title: "Negócios",
+    icon: QinAsset.FaceGlobe,
+  };
+  static REGION: AdModule = {
+    app: "adpeople",
+    title: "Região",
+    icon: QinAsset.FaceRegion,
+  };
+  static NATION: AdModule = {
+    app: "adpeople",
+    title: "Países",
+    icon: QinAsset.FaceGlobe,
+  };
+  static STATE: AdModule = {
+    app: "adpeople",
+    title: "Estados",
+    icon: QinAsset.FaceGlobe,
+  };
+  static CITY: AdModule = {
+    app: "adpeople",
+    title: "Cidades",
+    icon: QinAsset.FaceGlobe,
+  };
+  static DISTRICT: AdModule = {
+    app: "adpeople",
+    title: "Bairros",
+    icon: QinAsset.FaceGlobe,
+  };
+  static PEOPLE: AdModule = {
+    app: "adpeople",
+    title: "Pessoas",
+    icon: QinAsset.FaceGlobe,
+  };
+  static PEOPLE_GROUP: AdModule = {
+    app: "adpeople",
+    title: "Grupos de Pessoas",
+    icon: QinAsset.FaceGlobe,
+  };
+  static PEOPLE_SUBGROUP: AdModule = {
+    app: "adpeople",
+    title: "SubGrupos de Pessoas",
+    icon: QinAsset.FaceGlobe,
+  };
+}
---
+++ src/ad-names.ts
@@ -0,0 +1,8 @@
+export enum AdNames {
+  AdBoard = "AdBoard",
+  AdMister = "AdMister",
+  AdPeople = "AdPeople",
+  AdProduct = "AdProduct",
+  AdProject = "AdProject",
+  AdSales = "AdSales",
+}
