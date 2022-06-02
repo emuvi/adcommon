b059412a07051528f0e961b2e903a5196af931d9
Éverton M. Vieira <emuvi@outlook.com.br>
2022-06-02 17:41:39 -0300

    adds the linked class to make joins referenced on fields

--- src/ad-filter.ts
+++ src/ad-filter.ts
@@ -1,18 +1,18 @@
-import { AdNamed } from "./ad-named";
+import { AdLinked } from "./ad-linked";
 import { AdValued } from "./ad-valued";
 
 export class AdFilter {
   public seems: AdFilterSeems;
   public likes: AdFilterLikes;
   public valued: AdValued;
-  public named: AdNamed;
+  public linked: AdLinked;
   public ties: AdFilterTies;
 
   constructor(options?: AdFilterSet) {
     this.seems = options?.seems ?? AdFilterSeems.SAME;
     this.likes = options?.likes ?? AdFilterLikes.EQUALS;
     this.valued = options?.valued;
-    this.named = options?.named;
+    this.linked = options?.linked;
     this.ties = options?.ties ?? AdFilterTies.AND;
   }
 }
@@ -21,7 +21,7 @@ export type AdFilterSet = {
   seems?: AdFilterSeems;
   likes?: AdFilterLikes;
   valued?: AdValued;
-  named?: AdNamed;
+  linked?: AdLinked;
   ties?: AdFilterTies;
 };
 
